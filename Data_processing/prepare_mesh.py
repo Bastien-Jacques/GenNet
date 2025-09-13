b26351e9967eed1c6d36b59dbb4cee2d363365c8
Bastien-Jacques <bastien.jacques@ens-paris-saclay.fr>
2025-09-13 16:00:13 +0200

    Update and rename mesh_data.py to prepare_mesh.py

--- Data_processing/prepare_mesh.py
+++ Data_processing/prepare_mesh.py
@@ -3,34 +3,37 @@ import numpy as np
 import yaml
 import h5py
 
-# Charger le config.yaml
+## This code enables the transformation of the npz files encoding meshes of the vehicules into h5 files
+
+## We use this files during inference of the model in order to measure Chamer Distance between 
+## reconstructed shapes and ground truth
+
+# Loading config.yaml
 with open("/home/amb/bjacques/GenNet/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-val_path = config['data']['val_path']
+val_path = config['data']['val_path'] # example for validation
 with h5py.File(val_path, 'r') as f:
     shape_id = np.array(f['ids']).astype(str)
 
-# Dossier contenant les fichiers .npz (SDF de chaque forme)
+# folder containing npz files
 npz_folder = '/home/amb/bjacques/npz_files' 
 
-# Liste des fichiers .npz triés
+# list of sorted files
 npz_files = [f for f in np.sort(os.listdir(npz_folder)) if f.endswith('.npz')]
 
-# Chargement des ids de test set
+# Loadind of ids
 shape_id_val = set(shape_id) 
 
-# Dictionnaire résultat
+# Result dictionnary
 Result = {'shape_id': [], 'vertices': [], 'faces': []}
 
 for file in npz_files:
     filepath = os.path.join(npz_folder, file)
     data = np.load(filepath)
-
-    # Parcours des clés du fichier npz
     keys = list(data.keys())
 
-    # Supprimer les suffixes "_vertices" ou "_faces"
+    # delete "_vertices" ou "_faces"
     for k in keys:
         if k.endswith("_vertices"):
             base_name = k.replace("_vertices", "")
@@ -40,30 +43,10 @@ for file in npz_files:
                 Result['faces'].append(data[base_name + '_faces'])
 
 
-noms_vus = []
-for name in Result['shape_id']:
-    if name in noms_vus:
-        print(name)
-    noms_vus.append(name)
-
-
-shape_to_remove = 'N_S_WW_WM_349'
-if shape_to_remove in Result['shape_id']:
-    idx = Result['shape_id'].index(shape_to_remove)
-
-    # Supprimer aux bons indices dans toutes les listes
-    for key in Result:
-        del Result[key][idx]
-
-    print(f"{shape_to_remove} supprimé de Result.")
-else:
-    print(f"{shape_to_remove} non trouvé dans Result.")
-
-
-output_path = "/home/amb/bjacques/GenNet/data_split/val_mesh.h5"
+output_path = "YourOutputPath.h5"
 
 with h5py.File(output_path, 'w') as f:
-    shape_ids = [s.encode('utf-8') for s in Result['shape_id']]  # encodage en bytes
+    shape_ids = [s.encode('utf-8') for s in Result['shape_id']]  # bytes encoding
     f.create_dataset('shape_id', data=shape_ids)
 
     grp_vertices = f.create_group('vertices')
@@ -76,5 +59,5 @@ with h5py.File(output_path, 'w') as f:
         grp_vertices.create_dataset(shape_id, data=verts)
         grp_faces.create_dataset(shape_id, data=facs)
 
-print("Fichier HDF5 avec maillages variables sauvegardé avec succès.")
+print("file HDF5 with meshes saved with success.")
 
